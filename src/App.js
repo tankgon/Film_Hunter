10b0190bbfcea3d4af4845357f7fb335aa4516aa
ngocphuphamm <ngocphupham682001@gmail.com>
2022-07-01 03:19:06 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,46 +1,47 @@
 //redux
-import { Counter } from './features/counter/Counter';
+import { Counter } from "./features/counter/Counter";
 //css
-import './App.css';
-import './assets/ie6.css'
-import './assets/style.css'
+import "./App.css";
+import "./assets/ie6.css";
+import "./assets/style.css";
 //main
-import Layout from './main/layout'
+import Layout from "./main/layout";
 //layout
-import Home from './layout/Home'
-import Page from './layout/Page'
-import New from './layout/New'
-import FilmPage from './layout/FilmPage'
-import Chap from './layout/Chap'
+import Home from "./layout/Home";
+import Page from "./layout/Page";
+import New from "./layout/New";
+import FilmPage from "./layout/FilmPage";
+import Chap from "./layout/Chap";
 //component
-import Bug from './component/Bug';
-import DetailFilm from './component/DetailFilm';
-import Watching from './component/Watching';
+import Bug from "./component/Bug";
+import DetailFilm from "./component/DetailFilm";
+import Watching from "./component/Watching";
 //frameword
 import { Routes, Route, Navigate } from "react-router-dom";
-
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Layout/>}>
-        <Route path='' element={<Home/>}>
-          <Route path='' element={<Page/>}/>
+      <Route path="/" element={<Layout />}>
+        <Route path="" element={<Home />}>
+          <Route path="" element={<Page />} />
         </Route>
 
-        <Route path='/:id' element={<FilmPage/>}></Route>
-
-        <Route path='/Film/:id' element={<DetailFilm/>}/>
-
-        <Route path='/chap/:id' element={<Chap/>}>
-          <Route path='' element={<Watching/>}/>
+        <Route path="/:id" element={<FilmPage />}>
+          <Route path="" element={<Page />} />
         </Route>
-        
-        <Route path='/watching/:id' element={<Chap/>}/>
 
-        
+        <Route path="/Film/:id" element={<DetailFilm />} />
 
-        <Route path='/*' element={<Bug/>}></Route>
+        <Route path="/chap/:id" element={<Chap />}>
+          <Route path="/chap/:id/:tap" element={<Watching />} />
+        </Route>
+
+        {/* High Order Component */}
+
+        {/* <Route path='/watching/:id' element={<Chap/>}/> */}
+
+        <Route path="/*" element={<Bug />}></Route>
       </Route>
     </Routes>
   );
--- src/component/Watching.jsx
+++ src/component/Watching.jsx
@@ -1,34 +1,41 @@
-import React from 'react'
-import getAPI from '../api/phim'
+import React from "react";
+import getAPI from "../api/phim";
 import { Link, useParams } from "react-router-dom";
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 function Watching() {
-  const { id } = useParams();
-  const [listrenderTap, setlistrenderTap] = useState([]);
-
+  const { id, tap } = useParams();
+  const [ep, setEp] = useState({});
+  console.log(ep);
   useEffect(() => {
-      const getDetailFilmFC = async () => {
-          try {
-              const a = await getAPI.getDetailPhim(`${id}`);
-              setlistrenderTap(a.data.episodes);
-          } catch (error) {
-              alert(error);
-          }
-      };
-      getDetailFilmFC();
-  },[id])
+    const getDetailFilmFC = async () => {
+      try {
+        const a = await getAPI.getDetailPhim(`${id}`);
+        console.log(a.data);
+        const newEp = a.data.episodes[0].server_data.filter(
+          (el) => el.name === tap
+        )[0];
+        setEp(newEp);
+      } catch (error) {
+        alert(error);
+      }
+    };
+    getDetailFilmFC();
+  }, [id, tap]);
 
-  console.log(id);
+  // if (!id || !tap) return;
+
   return (
     <div>
-      {listrenderTap ? listrenderTap.map((item,index)=>{
-        return(                      
-          <iframe key={index} src={item.server_data[0].link_embed} width="100%" height="500px" controls>a</iframe>                                              
-      )})
-      :null}
+      <iframe
+        src={ep.link_embed}
+        width="100%"
+        height="500px"
+        controls
+        title={ep.filename}
+      ></iframe>
     </div>
-  )
+  );
 }
 
-export default Watching
+export default Watching;
